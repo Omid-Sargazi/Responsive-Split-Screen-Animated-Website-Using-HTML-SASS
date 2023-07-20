3e90aa57612be3488bb3ba0e2d2d39147055718f
OmidSargazi <om.sargazi@gmail.com>
2023-07-21 02:54:28 +0330

    use media Query for responsive website

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var menu = document.querySelector(".menu i");
+var clickShow = document.querySelector(".play-btn");
+var close = document.querySelector(".fa-times");
 function navToggle() {
   var nav = document.querySelector("nav");
   nav.classList.toggle("toggle");
   menu.classList.toggle("fa-times");
 }
 menu.addEventListener("click", () => navToggle());
+function imageToggle() {
+  var show = document.querySelector(".image-container");
+  var image = document.querySelector(".image img");
+  show.classList.toggle("toggleImage");
+}
+close.addEventListener("click", () => imageToggle());
+clickShow.addEventListener("click", () => imageToggle());
